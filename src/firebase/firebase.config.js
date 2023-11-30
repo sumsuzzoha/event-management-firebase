b1437c227e81316ea603529fafee53b73d5eaa50
Md. Sumsuzzoha <mdzoha11@gmail.com>
2023-12-01 01:43:09 +0600

    firebase config gitignore

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -14,12 +14,12 @@ import { initializeApp } from "firebase/app";
 // };
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAw2eMjXsgbefJ1lmb8Owrm3J4sWXRVP7c",
-    authDomain: "digi-session-event.firebaseapp.com",
-    projectId: "digi-session-event",
-    storageBucket: "digi-session-event.appspot.com",
-    messagingSenderId: "901093333008",
-    appId: "1:901093333008:web:72dcc89249ac84407b5295"
+    apiKey:import.meta.env.VITE_APIKEY,
+    authDomain:import.meta.env.VITE_AUTHDOMAIN,
+    projectId:import.meta.env.VITE_PROJECTID,
+    storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+    appId:import.meta.env.VITE_APPID,
   };
 
 // Initialize Firebase
